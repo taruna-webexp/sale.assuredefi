5680587ce6177c1fc636486bbc44eca0ce5c6ae5
taruna-webexp <taruna.webexpert@gmail.com>
2025-02-12 18:39:59 +0530

    added new code

--- src/services/apiCient.js
+++ src/services/apiCient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const ApiClient = () => {
   const baseURL =
-    "http://sales-api-assuredefi-27131a20e2d1.herokuapp.com/api/v1.0.1/";
+    "https://sales-api-assuredefi-27131a20e2d1.herokuapp.com/api/v1.0.1/";
 
   const instance = axios?.create({
     baseURL,
