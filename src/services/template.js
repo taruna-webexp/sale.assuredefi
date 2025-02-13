aeafa8683c2b38dd41187bcde1db85154afbb7e3
taruna-webexp <taruna.webexpert@gmail.com>
2025-02-13 16:51:28 +0530

    added api base url in env

--- src/services/template.js
+++ src/services/template.js
@@ -1,15 +1,12 @@
 export const createGoogleDoc = async (data) => {
   try {
-    const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbz1TE4Fqa4P1K3fR4kFss-JoKVNFmYVufDooyh80P1bVyGh2VSKpvu1BbaUMozT-5OJyg/exec",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "text/plain", // Change to text/plain to bypass CORS preflight
-        },
-        body: JSON.stringify(data),
-      }
-    );
+    const response = await fetch(process.env.NEXT_PUBLIC_GOOGLE_DOC_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify(data),
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
